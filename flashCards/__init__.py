a90391dc13df26e0cadb9c5807912d790cb7ff65
Adrian <adrianbennett333@gmail.com>
2024-01-19 14:05:11 +0000

    add app initiaisation function to db.py and call from __init__.py

--- flashCards/__init__.py
+++ flashCards/__init__.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask
 from dotenv import load_dotenv
+from . import db
 
 # Load environment variables from .env file if not already loaded
 load_dotenv(override=False)
@@ -30,5 +31,7 @@ def create_app(test_config=None):
         os.makedirs(app.instance_path)
     except OSError:
         pass
+
+    db.init_app(app)
 
     return app
